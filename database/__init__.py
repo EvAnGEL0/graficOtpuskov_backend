ecf2ee4151be268da508a762ed18756555522c69
EvAnGEL0 <vladmm007@gmail.com>
2025-09-16 10:27:55 +0300

    raund 3

--- database/__init__.py
+++
@@ -1,6 +0,0 @@
-# database/__init__.py
-from core.config import engine, AsyncSessionLocal, Base
-
-async def get_db():
-    async with AsyncSessionLocal() as session:
-        yield session
--- database/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# database/models/__init__.py
-from .user import User
-from .todo import Todo
-
-# Чтобы Alembic видел все модели
-__all__ = ["User", "Todo"]
